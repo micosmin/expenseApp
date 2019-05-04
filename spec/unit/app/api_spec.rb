86df803f378584b0c9d11297f2bee6c1a5c53686
Cosmin <mirceacosmin@gmail.com>
2019-05-04 13:07:16 +0100

    Unit test: sketched out unsucessfull API call

--- spec/unit/app/api_spec.rb
+++ spec/unit/app/api_spec.rb
@@ -4,12 +4,17 @@
 require_relative '../../../app/api.rb'
 
 module ExpenseTracker
-RSpec.describe API do
-  describe 'POST /expenses' do
-    context 'when the expense is sucessfully recorded' do
-      it 'returns the expense id'
-      it 'responds with a 200(OK'
+  RSpec.describe API do
+    describe 'POST /expenses' do
+      context 'when the expense is sucessfully recorded' do
+        it 'returns the expense id'
+        it 'responds with a 200(OK'
+      end
+
+      context 'when the expense fails validation' do
+        it 'returns an error message'
+        it 'responds with a 422 (Unprocessable entity'
+      end
     end
   end
-end
 end
